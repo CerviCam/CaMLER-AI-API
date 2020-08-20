85c2f3eb1448685dcc25e394551ba2f2c6158106
Nandhika Prayoga <nann011530@gmail.com>
2020-08-21 01:11:29 +0700

    Change the way to choose AI model

--- project/setting.py
+++ project/setting.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import json
 from dotenv import load_dotenv
 import torchvision.models as models
 from project.ai_model import AIModel
@@ -15,10 +16,10 @@ STORAGE_PATH = os.getenv('STORAGE_PATH', '/storage/')
 AI_MODELS_PATH = os.path.join(STORAGE_PATH, "models/")
 
 # Setup AI model
-model_file_name = (list(filter( 
-    lambda x: x.startswith("chosen"),
-    os.listdir(AI_MODELS_PATH)
-)) + [None])[0]
+model_file_name = None
+with open(os.path.join(AI_MODELS_PATH, '.config.json'), 'r') as config_file:
+    config = json.load(config_file)
+    model_file_name = config.pop("chosen")
 
 if model_file_name != None:
     AI_MODEL = AIModel(
